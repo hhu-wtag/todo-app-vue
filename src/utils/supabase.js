79b79cdbe6ef9c57ba746359707f5da983c08458
Hasibul Huda <hasibul.huda@welldev.io>
2022-05-11 09:40:33 +0600

    :rocket: Change deployment method

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -7,8 +7,8 @@ class Supabase {
     }
 
     this.connection = createClient(
-      process.env.VUE_APP_SUPABASE_URL,
-      process.env.VUE_APP_SUPABASE_KEY
+      "https://umzvvrrxvrkseldbmrxc.supabase.co",
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVtenZ2cnJ4dnJrc2VsZGJtcnhjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDY1NDk2NjQsImV4cCI6MTk2MjEyNTY2NH0.n8syuxaHjiGmC3R6DoFcuoqe6pNpkB--2BYSa_9d7fA"
     )
 
     Supabase.instance = this
